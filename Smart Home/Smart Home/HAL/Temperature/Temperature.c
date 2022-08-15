5f53184cd75372af5fc729daa11d3048be304e11
Nada Elgendy <76693809+nadaelgendy2@users.noreply.github.com>
2022-08-15 06:10:24 -0700

    Update Temperature.c

--- Smart Home/Smart Home/HAL/Temperature/Temperature.c
+++ Smart Home/Smart Home/HAL/Temperature/Temperature.c
@@ -1,1 +1,13 @@
 
+ #include "Temperature.h"
+ #include "ADC.h"
+ 
+ 
+ u16 Temperature_value(u16 ADC){
+	
+	 
+	 float mVolt = (ADC / 1023.0) * 5000;
+	 u16 Temperture= mVolt / 10;
+	 
+	 return Temperture;
+ }
